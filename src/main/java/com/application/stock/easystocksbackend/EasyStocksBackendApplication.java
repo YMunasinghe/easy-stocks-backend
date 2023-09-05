b397203ff3afdb7ebf02773dcf6250bf8eca5b57
Yasiru Munasinghe <yasiruchathu55@gmail.com>
2023-09-06 01:46:54 +0530

    solution for cross origin error

--- src/main/java/com/application/stock/easystocksbackend/EasyStocksBackendApplication.java
+++ src/main/java/com/application/stock/easystocksbackend/EasyStocksBackendApplication.java
@@ -2,12 +2,27 @@ package com.application.stock.easystocksbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EasyStocksBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyStocksBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
+			}
+		};
 	}
 
 }
